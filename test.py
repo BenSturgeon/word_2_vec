13c5ec39a164ac074203e2d7c16768d85a698370
Benjamin Sturgeon <sturgeonkid@gmail.com>
2022-12-08 19:24:54 +0200

    Successfully debugged forward pass. Edited dimensions to allow for matrix multiplcation.

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 import torch
 
-data = torch.ones(25,1)
+
+# 1. Create a tensor with 2 dimensions
+a = torch.tensor([[1, 2, 3], [4, 5, 6]])
+b = torch.tensor([[1, 2, 3], [4, 5, 6]])
+b.unsqueeze(2)
+mul_result = torch.mul(a, b)
+print(mul_result)
